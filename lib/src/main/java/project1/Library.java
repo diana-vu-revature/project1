1129b59ca00440923aa58971cfac4f4965dcf90a
diana-vu-revature <minhngoc.vu@revature.net>
2021-04-13 19:56:54 -0500

    servlet successfully imported

--- lib/src/main/java/project1/Library.java
+++ lib/src/main/java/project1/Library.java
@@ -6,10 +6,12 @@ package project1;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello")
 public class Library extends HttpServlet {
     public boolean someLibraryMethod() {
         return true;
@@ -17,7 +19,7 @@ public class Library extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        
+        resp.getWriter().println("Hello, there!");
     }
         
 }
